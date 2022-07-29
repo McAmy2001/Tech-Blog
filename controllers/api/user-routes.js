0148cc9acee83a4b24f8a5c7cdda9bbb5b46e524
Amy McCabe <mcca0168@umn.edu>
2022-07-29 17:20:11 -0500

    file structure outlined, user model added, db connected,  user routes started

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.get('/', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+  .then(dbUserData => {
+    res.json(dbUserData);
+  })
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
+
