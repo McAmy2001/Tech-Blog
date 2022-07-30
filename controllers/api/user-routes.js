39109d4153f3203de61cd388b2396b70ace6ade4
Amy McCabe <mcca0168@umn.edu>
2022-07-29 22:06:06 -0500

    initial user routes added and tested

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,7 +13,23 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
@@ -28,11 +44,41 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-
+  User.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData[0]) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
-
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
