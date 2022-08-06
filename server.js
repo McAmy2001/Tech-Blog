7740d86e51824f3becfce463a103363318dd4757
Amy McCabe <mcca0168@umn.edu>
2022-08-05 21:32:28 -0500

    cookie maxAge set

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: { maxAge: 120000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
